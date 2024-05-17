aa1b13244f8202ba6a4c4f8cfc97e5b1b17ad2fb
Pyth0nHater <116113690+Pyth0nHater@users.noreply.github.com>
2024-05-17 13:04:08 +0300

    fix reason id

--- src/constans.js
+++ src/constans.js
@@ -1,49 +1,49 @@
 export const arr = [
   {
-    id: 1,
+    id: 0,
     title: "18+",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat voluptas expedita aspernatur natus quo quis beatae laudantium earum voluptatum fugiat atque veritatis, maxime iure doloribus a enim magnam consectetur? Culpa unde commodi quis a aliquam exercitationem nihil neque cupiditate, accusamus in delectus consequuntur, deleniti dicta totam facilis illum libero, numquam ex quas eveniet. Quam quae consectetur fugiat doloremque exercitationem odit nostrum sapiente illo atque in ut sequi eveniet cum voluptatem, quibusdam rerum dolorum veritatis dolor sit sunt similique recusandae ab ipsam rem! Quos ratione harum non quis porro, distinctio consectetur esse odit tempore eius hic numquam nam rem cupiditate voluptate.",
   },
   {
-    id: 2,
+    id: 1,
     title: "Насилие",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat voluptas expedita aspernatur natus quo quis beatae laudantium earum voluptatum fugiat atque veritatis, maxime iure doloribus a enim magnam consectetur? Culpa unde commodi quis a aliquam exercitationem nihil neque cupiditate, accusamus in delectus consequuntur, deleniti dicta totam facilis illum libero, numquam ex quas eveniet. Quam quae consectetur fugiat doloremque exercitationem odit nostrum sapiente illo atque in ut sequi eveniet cum voluptatem, quibusdam rerum dolorum veritatis dolor sit sunt similique recusandae ab ipsam rem! Quos ratione harum non quis porro, distinctio consectetur esse odit tempore eius hic numquam nam rem cupiditate voluptate.",
   },
   {
-    id: 3,
+    id: 2,
     title: "Провокации",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat voluptas expedita aspernatur natus quo quis beatae laudantium earum voluptatum fugiat atque veritatis, maxime iure doloribus a enim magnam consectetur? Culpa unde commodi quis a aliquam exercitationem nihil neque cupiditate, accusamus in delectus consequuntur, deleniti dicta totam facilis illum libero, numquam ex quas eveniet. Quam quae consectetur fugiat doloremque exercitationem odit nostrum sapiente illo atque in ut sequi eveniet cum voluptatem, quibusdam rerum dolorum veritatis dolor sit sunt similique recusandae ab ipsam rem! Quos ratione harum non quis porro, distinctio consectetur esse odit tempore eius hic numquam nam rem cupiditate voluptate.",
   },
   {
-    id: 4,
+    id: 3,
     title: "Религия",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat voluptas expedita aspernatur natus quo quis beatae laudantium earum voluptatum fugiat atque veritatis, maxime iure doloribus a enim magnam consectetur? Culpa unde commodi quis a aliquam exercitationem nihil neque cupiditate, accusamus in delectus consequuntur, deleniti dicta totam facilis illum libero, numquam ex quas eveniet. Quam quae consectetur fugiat doloremque exercitationem odit nostrum sapiente illo atque in ut sequi eveniet cum voluptatem, quibusdam rerum dolorum veritatis dolor sit sunt similique recusandae ab ipsam rem! Quos ratione harum non quis porro, distinctio consectetur esse odit tempore eius hic numquam nam rem cupiditate voluptate.",
   },
 
   {
-    id: 5,
+    id: 4,
     title: "Запрещенные вещества",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat voluptas expedita aspernatur natus quo quis beatae laudantium earum voluptatum fugiat atque veritatis, maxime iure doloribus a enim magnam consectetur? Culpa unde commodi quis a aliquam exercitationem nihil neque cupiditate, accusamus in delectus consequuntur, deleniti dicta totam facilis illum libero, numquam ex quas eveniet. Quam quae consectetur fugiat doloremque exercitationem odit nostrum sapiente illo atque in ut sequi eveniet cum voluptatem, quibusdam rerum dolorum veritatis dolor sit sunt similique recusandae ab ipsam rem! Quos ratione harum non quis porro, distinctio consectetur esse odit tempore eius hic numquam nam rem cupiditate voluptate.",
   },
   {
-    id: 6,
+    id: 5,
     title: "Размер видео",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat voluptas expedita aspernatur natus quo quis beatae laudantium earum voluptatum fugiat atque veritatis, maxime iure doloribus a enim magnam consectetur? Culpa unde commodi quis a aliquam exercitationem nihil neque cupiditate, accusamus in delectus consequuntur, deleniti dicta totam facilis illum libero, numquam ex quas eveniet. Quam quae consectetur fugiat doloremque exercitationem odit nostrum sapiente illo atque in ut sequi eveniet cum voluptatem, quibusdam rerum dolorum veritatis dolor sit sunt similique recusandae ab ipsam rem! Quos ratione harum non quis porro, distinctio consectetur esse odit tempore eius hic numquam nam rem cupiditate voluptate.",
   },
   {
-    id: 7,
+    id: 6,
     title: "Качество видео",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat voluptas expedita aspernatur natus quo quis beatae laudantium earum voluptatum fugiat atque veritatis, maxime iure doloribus a enim magnam consectetur? Culpa unde commodi quis a aliquam exercitationem nihil neque cupiditate, accusamus in delectus consequuntur, deleniti dicta totam facilis illum libero, numquam ex quas eveniet. Quam quae consectetur fugiat doloremque exercitationem odit nostrum sapiente illo atque in ut sequi eveniet cum voluptatem, quibusdam rerum dolorum veritatis dolor sit sunt similique recusandae ab ipsam rem! Quos ratione harum non quis porro, distinctio consectetur esse odit tempore eius hic numquam nam rem cupiditate voluptate.",
   },
   {
-    id: 8,
+    id: 7,
     title: "Проблема ссылки",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat voluptas expedita aspernatur natus quo quis beatae laudantium earum voluptatum fugiat atque veritatis, maxime iure doloribus a enim magnam consectetur? Culpa unde commodi quis a aliquam exercitationem nihil neque cupiditate, accusamus in delectus consequuntur, deleniti dicta totam facilis illum libero, numquam ex quas eveniet. Quam quae consectetur fugiat doloremque exercitationem odit nostrum sapiente illo atque in ut sequi eveniet cum voluptatem, quibusdam rerum dolorum veritatis dolor sit sunt similique recusandae ab ipsam rem! Quos ratione harum non quis porro, distinctio consectetur esse odit tempore eius hic numquam nam rem cupiditate voluptate.",
