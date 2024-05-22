957a79484f47ce39c07e2e13305cc6c5fa2c81b2
Asliddin351 <nurillaev.asliddin@gmail.com>
2024-05-23 00:53:17 +0500

    delete flag from map

--- src/router/index.js
+++ src/router/index.js
@@ -290,8 +290,6 @@ const AddAdcView = () => import("@/views/client/AddAdcView.vue");
 const ClientSupportView = () => import("@/views/client/SupportView.vue");
 const ClientAnalyticsView = () => import("@/views/client/AnalyticsView.vue");
 const ClientFinanceView = () => import("@/views/client/FinanceView.vue");
-
-const ClientMapView = () => import("@/views/client/ClientMapView.vue");
 //
 
 //partner views
@@ -433,10 +431,6 @@ const routes = [
           {
             path: "support",
             component: ClientSupportView,
-          },
-          {
-            path: "map",
-            component: ClientMapView,
           },
         ],
       },
