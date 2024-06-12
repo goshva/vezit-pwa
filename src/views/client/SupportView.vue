c0d7178bca43c094c92235685f4820ca8c14d78a
iandypc <karpovandrey270896@yandex.ru>
2024-06-12 18:56:57 +0400

    define component RequestSupport.vue

--- src/views/client/SupportView.vue
+++ src/views/client/SupportView.vue
@@ -1,36 +1,6 @@
 <template>
-  <BaseBlock title="Обратная связь" class="m-5">
-    <div class="">
-      <!-- Sign In Form -->
-      <div class="mb-4">
-        Задайте свой вопрос:
-      </div>
-        <div class="mb-4">
-          <input
-              type="text"
-              class="form-control form-control-alt form-control-lg"
-              placeholder="Введите вопрос"
-            />
-          </div>
 
-          <div class="mb-4">
-            <strong>Наши контакты:
-            8(999)-555-55-55
-            example@mail.ru
-          </strong>
-          </div>
-
-          
-          <div class="mb-4">
-            <button type="submit" class="btn w-100 btn-alt-primary">
-              <i class=""></i>
-              Отправить
-            </button>
-          </div>
-        </div>
-        <!-- END Sign In Form -->
-  </BaseBlock>
-<!-- END ask question form -->
+<RequestSupport />
 
   <BaseBlock title="Обращения в техподдержку" class="m-5">
     <template #options>
@@ -193,5 +163,6 @@
 
 </template>
 <script setup>
+import RequestSupport from "@/components/RequestSupport.vue";
 </script>
 <style lang="scss"></style>
