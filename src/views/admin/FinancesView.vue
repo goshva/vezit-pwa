8c47a10d87239815d73bf6a3438f564b643bc722
goshva <sha.egor@gmail.com>
2024-09-01 22:50:19 +0300

    finaces fixs

--- src/views/admin/FinancesView.vue
+++ src/views/admin/FinancesView.vue
@@ -101,20 +101,30 @@ const changePage = (page) => {
                   <th>Время</th>
                   <th class="d-none d-sm-table-cell text-center">Описание</th>
                   <th class="d-none d-sm-table-cell text-end">Сумма</th>
-                  <th class="d-none d-sm-table-cell text-end">Состояние</th>
+                  <th class="d-none d-sm-table-cell text-end">Статус</th>
                 </tr>
               </thead>
               <tbody class="fs-sm">
                 <tr v-for="finance in finances" :key="finance.Event">
-                  <td>                    <p class="fs-sm fw-medium text-muted mb-0">
+                  <td>
+                    <p v-if="parseFloat(finance.Amount) < 0" class="fs-sm fw-medium text-muted mb-0">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="red" style="width: 32px">
                         <path stroke-linecap="round" stroke-linejoin="round"
                           d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                       </svg>
                       Списание
-                    </p></td>
-                  
+                    </p>
+                    <p v-if="parseFloat(finance.Amount) > 0" class="fs-sm fw-medium text-muted mb-0">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="green" class="w-3 h-3" style="width: 32px">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                          d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                      </svg>
+                      Пополнение
+                    </p>
+                  </td>
+
                   <td>
                     <p class="fs-sm fw-medium text-muted mb-0">
                       {{ finance.UpdatedAt }}
@@ -122,18 +132,17 @@ const changePage = (page) => {
                   </td>
                   <td class="d-none d-sm-table-cell fw-semibold text-muted">
                     {{ finance.Title }}
+                    <p class="fw-small mb-0">
+                      {{ finance.usernameD || "Имя пользователя" }}
+                    </p>
                   </td>
-                  <td>
-                    <span class="fs-xs fw-semibold d-inline-block py-1 px-3 rounded-pill" :class="{
-                      'bg-success-light text-success': parseInt(finance.Amount) > 0,
-                      'bg-danger-light text-danger': parseInt(finance.Amount) < 0,
-                    }">
-                      {{ finance.Amount }}
-                    </span>
+                  <td class="d-none d-sm-table-cell fw-semibold text-muted text-end">
+                    {{ finance.Amount }} ₽
                   </td>
 
                   <td class="d-none d-sm-table-cell text-end">
-                    <strong>{{ finance.price || '0' }}</strong>
+                    <i class="fa fa-fw fa-check text-success" v-if="parseInt(finance.Status) >0" title="Готово"></i>
+                    <i class="fas fa-spinner fa-spin" v-else title="В процессе"></i>
                   </td>
                 </tr>
               </tbody>
@@ -159,153 +168,6 @@ const changePage = (page) => {
         </div>
       </template>
     </BaseBlock>
-    <BaseBlock title="" class="mb-0">
-      <template #options>
-        <span class="fs-6 fw-semibold">Создать отчет</span>
-      </template>
-
-      <template #content>
-        <div class="block-content block-content-full">
-          <div class="table-responsive">
-            <table class="table table-hover table-vcenter">
-              <thead>
-                <tr>
-                  <th class="d-xl-table-cell">Событие</th>
-                  <th>Время</th>
-                  <th class="d-none d-sm-table-cell text-center">Описание</th>
-                  <th class="d-none d-sm-table-cell text-end">Сумма</th>
-                  <th class="d-none d-sm-table-cell text-end">Состояние</th>
-                </tr>
-              </thead>
-              <tbody class="fs-sm">
-                <tr>
-                  <td class="d-xl-table-cell">
-                    <p class="fs-sm fw-medium text-muted mb-0">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="green" class="w-3 h-3" style="width: 32px">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                          d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                      </svg>
-
-                      Пополнение
-                    </p>
-                  </td>
-                  <td>
-                    <p class="fs-sm fw-medium text-muted mb-0">
-                      11:00 12.05.2024
-                    </p>
-                  </td>
-                  <td class="d-none d-sm-table-cell">
-                    <p class="fs-xs fw-semibold mb-0">Через СБП</p>
-                  </td>
-                  <td class="d-none d-sm-table-cell fw-semibold text-muted text-end">
-                    5000 ₽
-                  </td>
-                  <td class="d-none d-sm-table-cell text-end">
-                    <p class="fs-xs fw-semibold mb-0">Проведено</p>
-                  </td>
-                </tr>
-                <tr>
-                  <td class="d-none d-xl-table-cell">
-                    <p class="fs-sm fw-medium text-muted mb-0">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="red" style="width: 32px">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                          d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                      </svg>
-                      Списание
-                    </p>
-                  </td>
-                  <td>
-                    <p class="fs-sm fw-medium text-muted mb-0">
-                      12:32 12.05.2024
-                    </p>
-                  </td>
-                  <td class="d-none d-sm-table-cell">
-                    <p class="fs-xs fw-semibold mb-0">
-                      Показ видео "Магазин цветов"
-                    </p>
-                  </td>
-                  <td class="d-none d-sm-table-cell fw-semibold text-muted text-end">
-                    0.1 ₽
-                  </td>
-                  <td class="d-none d-sm-table-cell text-end">
-                    <p class="fs-xs fw-semibold mb-0">Проведено</p>
-                  </td>
-                </tr>
-                <tr>
-                  <td class="d-none d-xl-table-cell">
-                    <p class="fs-sm fw-medium text-muted mb-0">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="red" style="width: 32px">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                          d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                      </svg>
-
-                      Списание
-                    </p>
-                  </td>
-                  <td>
-                    <p class="fs-sm fw-medium text-muted mb-0">
-                      11:00 12.05.2024
-                    </p>
-                  </td>
-                  <td class="d-none d-sm-table-cell">
-                    <p class="fs-xs fw-semibold mb-0">
-                      Показ видео "Магазин цветов"
-                    </p>
-                  </td>
-                  <td class="d-none d-sm-table-cell fw-semibold text-muted text-end">
-                    0.1 ₽
-                  </td>
-                  <td class="d-none d-sm-table-cell text-end">
-                    <p class="fs-xs fw-semibold mb-0">Проведено</p>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-        <!-- <div class="block-content block-content-full bg-body-light">
-          <nav aria-label="Photos Search Navigation">
-            <ul class="pagination pagination-sm justify-content-end mb-0">
-              <li class="page-item">
-                <a
-                  class="page-link"
-                  href="javascript:void(0)"
-                  tabindex="-1"
-                  aria-label="Previous"
-                >
-                  Prev
-                </a>
-              </li>
-              <li class="page-item active">
-                <a class="page-link" href="javascript:void(0)">1</a>
-              </li>
-              <li class="page-item">
-                <a class="page-link" href="javascript:void(0)">2</a>
-              </li>
-              <li class="page-item">
-                <a class="page-link" href="javascript:void(0)">3</a>
-              </li>
-              <li class="page-item">
-                <a class="page-link" href="javascript:void(0)">4</a>
-              </li>
-              <li class="page-item">
-                <a
-                  class="page-link"
-                  href="javascript:void(0)"
-                  aria-label="Next"
-                >
-                  Next
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div> -->
-      </template>
-    </BaseBlock>
-    <!-- END Recent Orders -->
   </div>
 </template>
 <script setup></script>
