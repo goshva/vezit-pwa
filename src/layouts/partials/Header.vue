c244d7e7c87ff51de482a5d482e54e1e0fa038e1
Erop <sha.egor@gmail.com>
2024-09-05 09:13:17 +0300

    fix chance role for admin

--- src/layouts/partials/Header.vue
+++ src/layouts/partials/Header.vue
@@ -6,7 +6,9 @@ import { useUserStore } from '@/stores/user'; // Import the user store from Pini
 const userStore = useUserStore();
 const username = computed(() => userStore.username);
 const roleName = computed(() => userStore.roleName);
-// Main store and Router
+const handleChangeRole = (rolePath) => {
+  router.push(`/${rolePath}`);
+};
 const store = useTemplateStore();
 const router = useRouter();
 const { path } = useRoute();
@@ -31,21 +33,15 @@ onMounted(() => {
           <div class="d-flex align-items-center">
             <slot name="content-left">
               <!-- Toggle Sidebar -->
-              <button
-                type="button"
-                class="btn btn-sm btn-alt-secondary me-2 d-lg-none"
-                @click="store.sidebar({ mode: 'toggle' })"
-              >
+              <button type="button" class="btn btn-sm btn-alt-secondary me-2 d-lg-none"
+                @click="store.sidebar({ mode: 'toggle' })">
                 <i class="fa fa-fw fa-bars"></i>
               </button>
               <!-- END Toggle Sidebar -->
 
               <!-- Open Search Section (visible on smaller screens) -->
-              <button
-                type="button"
-                class="btn btn-sm btn-alt-secondary d-md-none"
-                @click="store.headerSearch({ mode: 'on' })"
-              >
+              <button type="button" class="btn btn-sm btn-alt-secondary d-md-none"
+                @click="store.headerSearch({ mode: 'on' })">
                 <i class="fa fa-fw fa-search"></i>
               </button>
               <!-- END Open Search Section -->
@@ -62,106 +58,65 @@ onMounted(() => {
 
               <!-- User Dropdown -->
               <div class="dropdown d-inline-block ms-2">
-                <button
-                  type="button"
-                  class="btn btn-sm btn-alt-secondary d-flex align-items-center"
-                  id="page-header-user-dropdown"
-                  data-bs-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  <img
-                    class="rounded-circle"
-                    src="/assets/media/avatars/avatar10.jpg"
-                    alt="Header Avatar"
-                    style="width: 21px"
-                  />
-                  <span class="d-none d-sm-inline-block ms-2">{{username}}</span>
-                  <i
-                    class="fa fa-fw fa-angle-down d-none d-sm-inline-block opacity-50 ms-1 mt-1"
-                  ></i>
+                <button type="button" class="btn btn-sm btn-alt-secondary d-flex align-items-center"
+                  id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <img class="rounded-circle" src="/assets/media/avatars/avatar10.jpg" alt="Header Avatar"
+                    style="width: 21px" />
+                  <span class="d-none d-sm-inline-block ms-2">{{ username }}</span>
+                  <i class="fa fa-fw fa-angle-down d-none d-sm-inline-block opacity-50 ms-1 mt-1"></i>
                 </button>
-                <div
-                  class="dropdown-menu dropdown-menu-md dropdown-menu-end p-0 border-0"
-                  aria-labelledby="page-header-user-dropdown"
-                >
-                  <div
-                    class="p-3 text-center bg-body-light border-bottom rounded-top"
-                  >
-                    <img
-                      class="img-avatar img-avatar48 img-avatar-thumb"
-                      src="/assets/media/avatars/avatar10.jpg"
-                      alt="Header Avatar"
-                    />
+                <div class="dropdown-menu dropdown-menu-md dropdown-menu-end p-0 border-0"
+                  aria-labelledby="page-header-user-dropdown">
+                  <div class="p-3 text-center bg-body-light border-bottom rounded-top">
+                    <img class="img-avatar img-avatar48 img-avatar-thumb" src="/assets/media/avatars/avatar10.jpg"
+                      alt="Header Avatar" />
                     <p class="mt-2 mb-0 fw-medium">{{ username }}</p>
                     <p class="mb-0 text-muted fs-sm fw-medium">{{ roleName }}</p>
                   </div>
                   <div role="separator" class="dropdown-divider m-0"></div>
                   <div class="p-2">
-                    <RouterLink
-                      :to="{ name: 'auth-signin' }"
-                      class="dropdown-item d-flex align-items-center justify-content-between"
-                    >
+                    <RouterLink :to="{ name: 'auth-signin' }"
+                      class="dropdown-item d-flex align-items-center justify-content-between">
                       <span class="fs-sm fw-medium">Выйти</span>
                     </RouterLink>
                   </div>
                 </div>
               </div>
               <!-- END User Dropdown -->
-<!--Role Dropdown-->
-<div class="dropdown d-inline-block ms-2">
-  <button
-    type="button"
-    class="btn btn-sm btn-alt-secondary d-flex align-items-center"
-    id="page-header-user-dropdown"
-    data-bs-toggle="dropdown"
-    aria-haspopup="true"
-    aria-expanded="false"
-  >
-    <span class="d-sm-inline-block ms-2">Role</span>
-    <i
-      class="fa fa-fw fa-angle-down d-sm-inline-block opacity-50 ms-1 mt-1"
-    ></i>
-  </button>
-  <div
-    class="dropdown-menu dropdown-menu-md dropdown-menu-end p-0 border-0"
-    aria-labelledby="page-header-user-dropdown"
-  >
-    <div class="p-2">
-      <RouterLink
-        class="dropdown-item d-flex align-items-center justify-content-between"
-        to="/admin/dashboard"
-      >
-        <span class="fs-sm fw-medium">Администратор</span>
-      </RouterLink>
-      <button
-        @click="handleChangeRole('moderator/dashboard')"
-        class="dropdown-item d-flex align-items-center justify-content-between"
-      >
-        <span class="fs-sm fw-medium">Модератор</span>
-      </button>
-      <button
-        class="dropdown-item d-flex align-items-center justify-content-between"
-        @click="handleChangeRole('client/dashboard')"
-      >
-        <span class="fs-sm fw-medium">Клиент</span>
-      </button>
-      <button
-        class="dropdown-item d-flex align-items-center justify-content-between"
-        @click="handleChangeRole('partner/dashboard')"
-      >
-        <span class="fs-sm fw-medium">Партнёр</span>
-      </button>
-      <a
-        class="dropdown-item d-flex align-items-center justify-content-between"
-        href="javascript:void(0)"
-      >
-        <span class="fs-sm fw-medium">Гость</span>
-      </a>
-    </div>
-  </div>
-</div>
-<!--END Role Dropdown-->
+              <!--Role Dropdown-->
+              <div class="dropdown d-inline-block ms-2">
+                <button type="button" class="btn btn-sm btn-alt-secondary d-flex align-items-center"
+                  id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <span class="d-sm-inline-block ms-2">Role</span>
+                  <i class="fa fa-fw fa-angle-down d-sm-inline-block opacity-50 ms-1 mt-1"></i>
+                </button>
+                <div class="dropdown-menu dropdown-menu-md dropdown-menu-end p-0 border-0"
+                  aria-labelledby="page-header-user-dropdown">
+                  <div class="p-2">
+                    <RouterLink class="dropdown-item d-flex align-items-center justify-content-between"
+                      to="/admin/dashboard">
+                      <span class="fs-sm fw-medium">Администратор</span>
+                    </RouterLink>
+                    <button @click="handleChangeRole('moderator/dashboard')"
+                      class="dropdown-item d-flex align-items-center justify-content-between">
+                      <span class="fs-sm fw-medium">Модератор</span>
+                    </button>
+                    <button class="dropdown-item d-flex align-items-center justify-content-between"
+                      @click="handleChangeRole('client/dashboard')">
+                      <span class="fs-sm fw-medium">Клиент</span>
+                    </button>
+                    <button class="dropdown-item d-flex align-items-center justify-content-between"
+                      @click="handleChangeRole('partner/dashboard')">
+                      <span class="fs-sm fw-medium">Партнёр</span>
+                    </button>
+                    <a class="dropdown-item d-flex align-items-center justify-content-between"
+                      href="javascript:void(0)">
+                      <span class="fs-sm fw-medium">Гость</span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <!--END Role Dropdown-->
 
             </slot>
           </div>
@@ -172,11 +127,8 @@ onMounted(() => {
 
 
       <!-- Header Loader -->
-      <div
-        id="page-header-loader"
-        class="overlay-header bg-body-extra-light"
-        :class="{ show: store.settings.headerLoader }"
-      >
+      <div id="page-header-loader" class="overlay-header bg-body-extra-light"
+        :class="{ show: store.settings.headerLoader }">
         <div class="content-header">
           <div class="w-100 text-center">
             <i class="fa fa-fw fa-circle-notch fa-spin"></i>
