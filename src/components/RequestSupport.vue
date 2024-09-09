02b229d0c49f546ad6df654e529c63de7f573c7b
goshva <sha.egor@gmail.com>
2024-09-09 14:51:43 +0300

    add support meaages

--- src/components/RequestSupport.vue
+++ src/components/RequestSupport.vue
@@ -1,17 +1,54 @@
 <script setup>
-defineProps(['whoRequestsId'])
+
+import { ref, onMounted } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import axiosInstance from "@/services/axios.js";
+
+const route = useRoute();
+const router = useRouter();
+
+// Message details
+const supportMessage = ref({
+  userId: 1,
+  message: "",
+  support_id:1,
+});
+
+// Fetch support details if necessary
+const fetchSupportDetails = async () => {
+  try {
+    const response = await axiosInstance.get(`/support`);
+    console.log('Fetched support details:', response.data);
+  } catch (error) {
+    console.error("Error fetching support details:", error);
+  }
+};
+
+// Submit the support message
+const handleSubmit = async () => {
+  try {
+    await axiosInstance.post(`/support`, supportMessage.value);
+  } catch (error) {
+    console.error("Error sending message to support:", error);
+  }
+};
+
+onMounted(() => {
+  fetchSupportDetails(route.params.id);
+});
 </script>
 
 <template>
 
   <BaseBlock title="Обратная связь" class="m-5">
     <div class="">
-      <!-- Sign In Form -->
+      <!-- Support Form -->
       <div class="mb-4">
         Задайте свой вопрос:
       </div>
       <div class="mb-4">
         <input
+          v-model="supportMessage.message"
           type="text"
           class="form-control form-control-alt form-control-lg"
           placeholder="Введите вопрос"
@@ -26,8 +63,7 @@ defineProps(['whoRequestsId'])
       </div>
 
       <div class="mb-4">
-        <button type="submit" class="btn w-100 btn-alt-primary">
-          <i class=""></i>
+        <button type="submit" class="btn w-100 btn-alt-primary" @click="handleSubmit">
           Отправить
         </button>
       </div>
