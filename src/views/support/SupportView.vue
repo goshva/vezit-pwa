6ae09bd628b49dd9bb7bec8dd681db2a690a0001
boank <boankartal@gmail.com>
2024-12-06 15:24:31 +0300

    changed the view of the technical support table

--- src/views/support/SupportView.vue
+++ src/views/support/SupportView.vue
@@ -33,10 +33,7 @@ const fetchEquipments = async (page = 1, status = '') => {
 onMounted(() => {
   fetchEquipments();
 });
-const applyFilter = (status) => {
-  filterStatus.value = status;
-  fetchEquipments(1, status);
-};
+
 
 const changePage = (page) => {
   fetchEquipments(page, filterStatus.value);
@@ -46,42 +43,6 @@ const changePage = (page) => {
 <template>
   <div class="m-5 mb-0">
     <BaseBlock title="Обращения в техподдержку" class="mb-0">
-      <template #options>
-        <div class="space-x-1">
-          <div class="dropdown d-inline-block">
-            <button type="button" class="btn btn-sm btn-alt-secondary" id="dropdown-recent-orders-filters"
-              data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="fa fa-fw fa-flask"></i>
-              Фильтр
-              <i class="fa fa-angle-down ms-1"></i>
-            </button>
-            <div class="dropdown-menu dropdown-menu-md dropdown-menu-end fs-sm"
-              aria-labelledby="dropdown-recent-orders-filters">
-              <a class="dropdown-item fw-medium d-flex align-items-center justify-content-between"
-                href="javascript:void(0)" @click.prevent="applyFilter('')">
-                Все
-                <span class="badge bg-primary rounded-pill">{{ messages.length }}</span>
-              </a>
-              <a class="dropdown-item fw-medium d-flex align-items-center justify-content-between"
-                href="javascript:void(0)" @click.prevent="applyFilter('in-progress')">
-                В работе
-                <span class="badge bg-primary rounded-pill">72</span>
-              </a>
-              <a class="dropdown-item fw-medium d-flex align-items-center justify-content-between"
-                href="javascript:void(0)" @click.prevent="applyFilter('completed')">
-                Готово
-                <span class="badge bg-primary rounded-pill">890</span>
-              </a>
-              <a class="dropdown-item fw-medium d-flex align-items-center justify-content-between"
-                href="javascript:void(0)" @click.prevent="applyFilter('error')">
-                Ошибка
-                <span class="badge bg-primary rounded-pill">997</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </template>
-
       <template #content>
         <div v-if="loading" class="block-content text-center">
           <span>Загрузка сообщений...</span>
@@ -91,39 +52,36 @@ const changePage = (page) => {
             <table class="table table-hover table-vcenter">
               <thead>
                 <tr>
-                  <th class="d-none d-sm-table-cell text-center">Сообщение / Ответ</th>
-                  <th class="d-none d-sm-table-cell text-center">Время</th>
-                  <th class="d-none d-sm-table-cell text-end">Статус</th>
-                  <th class="d-none d-sm-table-cell text-end"></th>
+                  <th class="d-none d-sm-table-cell text-center">№ Обращения</th>
+                  <th class="d-none d-sm-table-cell text-center">Создатель запроса</th>
+                  <th class="d-none d-sm-table-cell text-center">Дата и время создания</th>
+                  <th class="d-none d-sm-table-cell text-center">Дата и время ответа</th>
+                  <th class="d-none d-sm-table-cell text-center">Статус</th>
                 </tr>
               </thead>
               <tbody class="fs-sm">
                 <tr v-for="message in messages" :key="message.id">
-                  <td class="d-none d-sm-table-cell fw-semibold text-muted">
-                    <p class="fw-medium mb-0">
-                      {{ message.user_id }} : «{{ message.message }}»
+                  <td class="d-none d-sm-table-cell fw-semibold text-muted text-center">
+                    {{message.id}}
+                  </td>
+                  <td>
+                    <p class="fs-sm fw-medium text-muted mb-0 text-center">
+                      {{ message.user.username }}
                     </p>
-                    <p v-if="message.support_answer" class="fw-medium
-                     mb-0">
-                      {{ message.support_id }} : «{{ message.support_answer }}»
+                  </td>
+                  <td>
+                    <p class="fs-sm fw-medium text-muted mb-0 text-center">
+                      {{ formatDate(message.created_at) }}
                     </p>
-
                   </td>
                   <td>
                     <p class="fs-sm fw-medium text-muted mb-0 text-center">
                       {{ formatDate(message.updated_at) }}
                     </p>
                   </td>
-                  <td class="d-none d-sm-table-cell text-end">
+                  <td class="d-none d-sm-table-cell text-center">
                     <i class="fa fa-fw fa-check text-success" v-if="parseInt(message.status) > 0" title="Готово"></i>
                     <i class="fas fa-spinner fa-spin" v-else title="В процессе"></i>
-                  </td>
-                  <td class="d-sm-table-cell fw-semibold text-muted text-end">
-                    <router-link :to="{ name: 'ResponseSupport', params: { id: message.id } }">
-                      <button class="btn btn-sm btn-alt-primary">
-                        <i class="fa fa-edit"></i>
-                      </button>
-                    </router-link>
                   </td>
                 </tr>
               </tbody>
