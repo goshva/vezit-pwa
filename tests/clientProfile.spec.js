a5c8d08dca4dc5c201dc44e187422b2bb6cb5ac2
paww <122092302+pawwvw@users.noreply.github.com>
2025-01-06 14:25:32 +0300

    сделал автотесты для профиля клиенты

---
+++ tests/clientProfile.spec.js
@@ -0,0 +1,38 @@
+import { test, expect } from '@playwright/test'
+
+const role = {name: "client", firstURL: "profile", password: "123456", email: "client@testsystem.ru"}
+
+test.describe('Send client info functionality', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('http://localhost:5173/#/auth/signin');
+        await page.fill('input[name="login-email"]', role.email);
+        await page.fill('input[name="login-password"]', role.password);
+        await page.click('button[type="submit"]');
+        await expect(page).toHaveURL(`http://localhost:5173/#/${role.firstURL}`);
+        await page.goto('http://localhost:5173/#/profile')
+        await expect(page).toHaveURL('http://localhost:5173/#/profile')
+    });
+
+    test('should successfully send info about client without checkbox', async ({page}) => {
+        await expect(page).toHaveURL('http://localhost:5173/#/profile');
+        await page.fill('input[placeholder="Наименование компании"]', Math.random().toString(36).substring(2, 10));
+        await page.fill('input[placeholder="Вид деятельности"]', Math.random().toString(36).substring(2, 10));
+        await page.fill('input[placeholder="ОГРН/ОГРНИП"]', Math.random().toString(36).substring(2, 10));
+        await page.fill('input[placeholder="Р/с"]', Math.random().toString(36).substring(2, 10));
+        await page.fill('input[placeholder="БИК"]', Math.random().toString(36).substring(2, 10));
+        await page.fill('input[placeholder="Контактное лицо"]', Math.random().toString(36).substring(2, 10));
+        await page.fill('input[placeholder="Телефон контактного лица"]', Math.random().toString(36).substring(2, 10));
+        await page.fill('input[placeholder="ФИО руководителя"]', Math.random().toString(36).substring(2, 10));
+        await page.fill('input[placeholder="Телефон руководителя"]', Math.random().toString(36).substring(2, 10));
+        await page.fill('input[placeholder="Электронная почта"]', Math.random().toString(36).substring(2, 10) + '@test.com');
+        await page.fill('textarea[placeholder="Описание деятельности"]', Math.random().toString(36).substring(2, 10));
+        await page.check('#publicOfferConsent');
+        const response = await Promise.all([
+            page.click('button.btn.w-100'),
+            page.waitForResponse(response => response.status() === 200)
+        ]);
+        expect(response[1].ok()).toBeTruthy();
+    });
+})
+    
+    
