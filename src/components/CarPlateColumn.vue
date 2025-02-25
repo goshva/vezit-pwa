f5d3de3f9908b2374ae55020b9d6a9c817a6fc5d
MynameA2 <158841089+mynameA2@users.noreply.github.com>
2025-02-25 10:48:25 +0500

    новый компонент для столбца номер

---
+++ src/components/CarPlateColumn.vue
@@ -0,0 +1,27 @@
+<template>
+    <td class="d-none d-sm-table-cell text-start car-plate">
+        <p v-if="plate" class="mb-0">{{plate}}</p>
+    </td>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    plate: {
+        type: String,
+        required: false,
+        default: ''
+    }
+});
+</script>
+
+<style lang="scss">
+.car-plate p {
+  border: 1px solid #ccc;
+    border-radius: 5px;
+    font-weight: bold;
+    display: flex;
+    justify-content: center;
+}
+</style>
